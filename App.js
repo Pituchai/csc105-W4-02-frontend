d7a2ddcb0bffcaf1ec88b26720c3834ff77da91b
Pituchai <Phantom_14706@hotmail.com>
2022-05-06 14:59:14 +0700

    test

---
+++ App.js
@@ -0,0 +1,4 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';  <Menu /> 
+
---
+++ index.js
@@ -0,0 +1,4 @@
+import  'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
